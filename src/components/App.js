249a6d8389bcdbc55ae6971ce13161b449ac33be
kimyonghee1216 <kimyh1216@kakao.com>
2022-08-31 21:54:41 +0900

    authService 작업중

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import '../App.css';
 import React,{useState} from 'react';
-import Router from './Router'
+import Router from 'components/Router'
+import {authService} from 'fbase'
 
 function App() {
+  console.log(authService.currentUser)
+  const [isLoggedIn,setIsLoggedIn] = useState(false)
   return (
     <div>
-      <Router />
+      <Router isLoggedIn={isLoggedIn} />
+      <footer>&copy; Nwitter {new Date().getFullYear()} </footer>
     </div>
   );
 }
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,9 @@
 import React, {useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Auth from '../routes/Auth'
-import Home from '../routes/Home'
+import Auth from 'routes/Auth'
+import Home from 'routes/Home'
 
-const Router = () =>{
-    const [isLoggedIn,setIsLoggedIn] = useState(false)
+const Router = ({isLoggedIn}) =>{
     return(
         <BrowserRouter>
             <Routes>
