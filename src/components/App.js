b5c0883d62e1eb95337956556f67082520cfe6f3
kimyonghee1216 <kimyh1216@kakao.com>
2022-09-02 20:47:23 +0900

    add email login function

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import '../App.css';
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import Router from 'components/Router'
 import {authService} from 'fbase'
 
 function App() {
-  console.log(authService.currentUser)
-  const [isLoggedIn,setIsLoggedIn] = useState(authService.currentUser)
+  const [init,setInit] = useState(false)
+  const [isLoggedIn,setIsLoggedIn] = useState(false)
+  useEffect(()=>{
+    authService.onAuthStateChanged((user)=>{
+      if(user){
+        setIsLoggedIn(true)
+      }else{
+        setIsLoggedIn(false)
+      }
+      setInit(true)
+      })
+    },[])
   return (
     <div>
-      <Router isLoggedIn={isLoggedIn} />
+      {init ? <Router isLoggedIn={isLoggedIn} /> : 'Initializing...' }
       <footer>&copy; Nwitter {new Date().getFullYear()} </footer>
     </div>
   );
