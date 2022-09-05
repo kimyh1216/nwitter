51cf8c72ab0efd5cd6abdd1095443b218d45359c
kimyonghee1216 <kimyh1216@kakao.com>
2022-09-05 21:22:51 +0900

    add delete function

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from "components/Nweet";
 import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
 
@@ -35,10 +36,9 @@ const Home = ({userObj}) => {
             <input type='submit' value='Nweet' />
         </form>
         <div>
-            {nweets.map(nweet => 
-            <div key={nweet.id}>
-                <h4>{nweet.text}</h4>
-            </div>)}
+            {nweets.map((nweet) => 
+                <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
+            )}
         </div>
     </div>
     )
