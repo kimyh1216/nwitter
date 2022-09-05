fd39a0f2bc08f58b0ac292836f6fe4672c8eeb95
kimyonghee1216 <kimyh1216@kakao.com>
2022-09-05 20:25:33 +0900

    edit db service get function

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet,setNweet] = useState('')
+    const [nweets,setNweets] = useState([])
+    const getNweets = async()=>{
+        const dbNweets = await dbService.collection('nweets').get()
+        //console.log(dbNweets)
+        dbNweets.forEach(document => {
+            const nweetObject = {
+                ...document.data(),
+                id:document.id
+            }
+            setNweets((prev)=>[nweetObject,...prev])
+        })
+    }
+    useEffect(()=>{
+        getNweets()
+    },[])
     const onSubmit = async(event) =>{
         event.preventDefault()
         await dbService.collection('nweets').add({
@@ -15,12 +30,19 @@ const Home = () => {
         const {target:{value}}=event
         setNweet(value)
     }
+    console.log(nweets)
     return(
     <div>
         <form onSubmit={onSubmit}>
             <input value={nweet} onChange={onChange} type='text' placeholder="What's on your mind?" maxLength={120} />
             <input type='submit' value='Nweet' />
         </form>
+        <div>
+            {nweets.map(nweet => 
+            <div key={nweet.id}>
+                <h4>{nweet.nweet}</h4>
+            </div>)}
+        </div>
     </div>
     )
 }
